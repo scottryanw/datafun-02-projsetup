f42c02531735de7fd7743ca2888d455ca5289449
Scott Williamson <157340660+scottryanw@users.noreply.github.com>
2024-05-12 11:31:36 -0500

    Week 1 project upload

--- Williamson_projsetup.py
+++ Williamson_projsetup.py
@@ -1,1 +1,17 @@
-'''Week 2 Project Script'''
+'''Start a data analytics project.'''
+import pathlib
+
+def create_project_directory(directory_name: str): 
+    '''
+    Creates a project sub-directory.
+    :param directory_name: Name of the directory to be created, e.g. "test"
+    '''
+    pathlib.Path(directory_name).mkdir(exist_ok=True)
+
+def main(): 
+    ''' Scaffold a project. '''
+    create_project_directory(directory_name='test') 
+    create_project_directory(directory_name='docs') 
+
+if __name__ == '__main__':
+    main()
